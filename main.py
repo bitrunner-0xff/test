cb21f6b9ca4b2d259411b405db41d5a853dd71f5
glen <mrflint46@gmail.com>
2025-01-10 10:14:46 +0100

    fixed run script

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import lib
+import lib.core
+
 class Fuck:
-    def func(fuck = 10):
+    def func(self, fuck = 10):
         print(f"fuck you {fuck} times")
+        self.helper()
         print([i * i for i in range(10, 1000)])
+
+    def helper(self, varr):
+        lib.core()
 
 
 if __name__ == "__main__":
