ecc3f7507f9b1a817d93ba91f84c6fd7b8c519bd
glen <mrflint46@gmail.com>
2025-01-10 09:32:41 +0100

    fixed mistakes and added new tests

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 def test_func_1():
     pass
 
-def test_func_2()
+def test_func_2():
     pass
 
-def test_func_3()
+def test_func_3():
     pass
+
+def test_func_4():
+    pass
+
+def test_func_5():
+    pass
+
+def test_func_6():
+    pass
+
