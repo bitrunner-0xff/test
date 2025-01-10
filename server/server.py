b88c27288260cbf16885d07bb3c884c1bc97cc0b
glen <mrflint46@gmail.com>
2025-01-10 17:25:01 +0100

    fixed server

--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
 class Server:
-    def __init__(self):
-        pass
+    def __init__(self, ip):
+        self.ip = ip
 
     def start(self):
         print("start server", end="\n")
-        
+
+    def activate(self):
+        print(f"activate server {ip}.")
